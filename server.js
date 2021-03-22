34a0c24a424e5f6c18b15f560f94a355bfb1f418
LIJOMLOYID <lijomloyid@gmail.com>
2021-03-22 14:24:43 +0530

    Implemented message broadcast to all connected clients

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const io = require("socket.io")(3000, {
   },
 });
 io.on("connection", (socket) => {
-  console.log("New User");
-  socket.emit("chat-message", "Hello World");
+  // console.log("New User");
+  // socket.emit("chat-message", "Hello World");
   socket.on("send-chat-message", (message) => {
-    console.log(message);
+    socket.broadcast.emit("chat-message", message);
   });
 });
